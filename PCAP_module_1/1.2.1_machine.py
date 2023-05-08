6938baedc3a821497631a17ca4563ca7203120ce
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2023-05-08 22:48:26 +0200

    fix: add PCAP 1.2.1 machine and platform

---
+++ PCAP_module_1/1.2.1_machine.py
@@ -0,0 +1,5 @@
+from platform import machine
+'''
+Print CPU architecture
+'''
+print(machine())
---
+++ PCAP_module_1/1.2.1_platform.py
@@ -0,0 +1,9 @@
+from platform import platform
+
+'''
+Print OS platform
+'''
+
+print(platform())
+print(platform(1))
+print(platform(0, 1))
