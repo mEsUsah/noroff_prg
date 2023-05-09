63d88c76c5e9c2aee3c4b1fd507df32977b88142
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2023-05-09 22:47:20 +0200

    fix: refactor 422, and include docstring

--- PRG_module_4/422_guessingGame.py
+++ PRG_module_4/422_guessingGame.py
@@ -1,6 +1,21 @@
 import moduleTwo as myModules
 
-if __name__ == '__main__':
+def task():
+    '''
+    In this script, you need to access the functions created in the mymodules.py script. 
+    Furthermore, this script should present the user with a guessing game. 
+    The user should be prompted to guess a number between 0 and 10. 
+    The system should then randomly generate a number between 0 and 10 using a function 
+    from mymodules.py. 
+    
+    If the user guessed correctly, display a congratulatory message. 
+    Otherwise, display a message stating the actual number and wish them luck for their next attempt. 
+    The user should be able to play the game as many times as they want. 
+    
+    If the user has finished playing, the system should display a message stating how many 
+    seconds they played.
+    '''
+
     start_time = myModules.create_time()
     while True:
         number = myModules.generate_random(1,10)
@@ -20,3 +35,6 @@ if __name__ == '__main__':
     diff_time = myModules.calculate_difference(start_time,end_time)
     print("Thank you for playing! :D")
     print(f"You played for {diff_time.seconds} seconds")
+
+if __name__ == '__main__':
+    task()
