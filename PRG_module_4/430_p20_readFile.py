8ad7d02c1141fe258e409b43b8db3284193ff058
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2023-05-10 22:49:51 +0200

    fix: add task text to 430_p20 and 430_p21

--- PRG_module_4/430_p20_readFile.py
+++ PRG_module_4/430_p20_readFile.py
@@ -1,21 +1,38 @@
-file_path = 'PRG_module_4/myfileOne.txt'
+def task():
+    '''
+    Write a Python script that opens a text file, reads its contents, and displays 
+    it to the command-line. 
+    
+    After displaying the contents, the script should request values from a user 
+    continuously until they choose to stop. 
+    
+    All values entered should be appended to the same text file of which the contents 
+    were displayed at the start of the script’s execution. 
+    
+    Ensure that you run the script multiple times to see how the number of values 
+    in the file increases.
+    '''
+    file_path = 'PRG_module_4/myfileOne.txt'
 
-my_file = open(file_path,'r')
-file_content =  my_file.readlines()
-for line in file_content:
-    print(line, end='')
+    my_file = open(file_path,'r')
+    file_content =  my_file.readlines()
+    for line in file_content:
+        print(line, end='')
 
-my_file.close()
+    my_file.close()
 
-my_file = open(file_path,'a')
-print("#"*120)
-print("Write, and press enter to add new text. to quit, write 'q'")
-print("#"*120)
-while True:
-    line = input()
-    if line == "q":
-        break
-    my_file.write(f'{line}\n')
+    my_file = open(file_path,'a')
+    print("#"*120)
+    print("Write, and press enter to add new text. to quit, write 'q'")
+    print("#"*120)
+    while True:
+        line = input()
+        if line == "q":
+            break
+        my_file.write(f'{line}\n')
 
-my_file.close()
+    my_file.close()
+
+if __name__ == '__main__':
+    task()
 
--- PRG_module_4/430_p21_textSeek.py
+++ PRG_module_4/430_p21_textSeek.py
@@ -1,4 +1,12 @@
-my_file = open('PRG_module_4/myfileTwo.txt','r+')
+def task():
+    '''
+    Use a text editor to create a text file consisting of multiple lines of text. 
+    Create a script that demonstrates how the Python seek() method can be used to 
+    modify the contents of the file.
+    '''
+    my_file = open('PRG_module_4/myfileTwo.txt','r+')
+    my_file.seek(10)
+    my_file.write("CHANGED!")
 
-my_file.seek(10)
-my_file.write("CHANGED!")
+if __name__ == '__main__':
+    task()
