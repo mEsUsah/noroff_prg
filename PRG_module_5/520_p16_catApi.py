a32e04af18aee07078aeffee6dba8fe865e179d0
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2023-05-26 11:31:27 +0200

    fix: add p520_p16

---
+++ PRG_module_5/520_p16_catApi.py
@@ -0,0 +1,28 @@
+import requests
+from configparser import ConfigParser
+
+# Get API key form .env file
+config = ConfigParser()
+config.read('.env')
+API_KEY = config['API']['CAT']
+
+def task():
+    '''
+    Visit The Cat API - Cats as a Service and try to create a Python API call to 
+    request information regarding a specific cat breed.
+    '''
+    pass
+
+    url = 'https://api.thecatapi.com/v1/breeds/abys'
+    headers = {
+        'x-api-key': API_KEY
+    }
+
+    response = requests.get(url, headers=headers)
+    cat_info = response.json()
+    for key, value in cat_info.items():
+        print(f'{key}: {value}')
+
+
+if __name__ == '__main__':
+    task()
