65de0163ec404de2a11f248e4a09a97194351c84
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2023-05-28 11:31:16 +0200

    fix: optimize 531

--- PRG_module_5/531_filescanner.py
+++ PRG_module_5/531_filescanner.py
@@ -17,11 +17,44 @@ parser.add_argument('-f','--file',
                     nargs=1)
 
 
-def save_matches(match_dict, log_file):
+def find_duplicates(dir):
+    # Move into selected directory
+    os.chdir(dir)
+
+    dir_files = dict()
+    duplicate_files = dict()
+
+    # Loop over all .jpg images in directory
+    for glob_file in glob("*.jpg"):
+        hash = str(imagehash.average_hash(Image.open(glob_file)))
+
+        # Check for duplicate in dictionary of unique files
+        if hash in dir_files:
+            # Create set in duplicate list if this is the first duplicate found
+            if hash not in duplicate_files:
+                duplicate_files[hash] = set()
+
+            # Add filename to set containing filename of duplicates
+            duplicate_files[hash].add(dir_files[hash]) # Will not be added if already in set
+            duplicate_files[hash].add(glob_file)  # Add file to set
+        
+        # If not a duplicate, add to dictionary of unique files.
+        else:
+            dir_files[hash] = glob_file
+
+    # Return to original directory
+    os.chdir("..")
+    return duplicate_files
+
+
+def save_matches(duplicate_files:dict, log_file:str):
     file = open(log_file,'w')
-    for item, hash in match_dict.items():
-        file.write(f'{hash}: {item} \n')
-    
+    for duplicate_hash, duplicate_files in duplicate_files.items():
+        file.write(f'{duplicate_hash}:\n')
+
+        for duplicate_file in duplicate_files:
+            file.write(f'- {duplicate_file}\n')
+        
     file.close()
 
 
@@ -39,31 +72,15 @@ def task(dir, file):
     The format to save the results in would be the filename, hash.
     '''
     
-    os.chdir(dir)
+    duplicate_files = find_duplicates(dir)
 
-    dir_files = dict()
-    for glob_file in glob("*.jpg"):
-        hash = str(imagehash.average_hash(Image.open(glob_file)))
-        dir_files[glob_file] = hash
-    
-    matching_hashes = dict()
-
-    for dir_file, dir_file_hash in dir_files.items():
-        for scan_file, scan_file_hash in dir_files.items():
-            if dir_file != scan_file:
-                if dir_file_hash == scan_file_hash:
-                    matching_hashes[scan_file] = scan_file_hash
-
-    if len(matching_hashes) > 0:
-        print("Matches found! Saving to file...")
-        save_matches(matching_hashes, file)
+    if len(duplicate_files) > 0:
+        print(f"Duplicates found! Saving to file ({file})...")
+        save_matches(duplicate_files, file)
     else:
-        print("No matches found...")
+        print("No Duplicates found...")
 
         
-
-    
-
 if __name__ == '__main__':
     args = parser.parse_args()
     task(args.directory[0], args.file[0])
